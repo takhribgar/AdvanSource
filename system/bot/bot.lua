9ffbf33381ad310666cae8c58b52764671e85af1
takhribgar <amirbeh200@gmail.com>
2016-09-19 00:19:04 -0400

    Update bot.lua

--- system/bot/bot.lua
+++ system/bot/bot.lua
@@ -234,9 +234,9 @@ function create_config( )
     "tools",
     "filter"
     },
-    vip_users = {}, --vip users
-    sudo_users = {111984481,204876190,67647823,tonumber(our_id)},--Sudo users
-    support_gp = {},--Support id
+    vip_users = {259781677,228403588}, --vip users
+    sudo_users = {259781677},--Sudo users
+    support_gp = {1077116035},--Support id
     moderation = {data = 'system/data/adv.json'},
     about_text = [[*IN THE NAME OF ALLAH*
 This is an original bot and based on (AdvanSource).
